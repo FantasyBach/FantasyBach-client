52f482a566a7b12fcd89cc15ed4d9591d78a4a77
Nicholas Clawson <nickclaw@gmail.com>
2015-11-21 15:19:19 -0800

    fix dev server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var statics = require('serve-static');
 app.use('/static', statics('build'));
-app.use(statics('index.html'));
+app.use(statics('.'));
 app.listen(8080, function(err) {
     if (err) console.error("error", err);
     else console.log('listening on port 8080');
