c599a9543e315893a4e67476177aecdf58dc3a95
Nicholas Clawson <nickclaw@gmail.com>
2015-12-20 21:11:46 -0800

    ViewContestant page

--- src/js/routes/ViewContestant.js
+++ src/js/routes/ViewContestant.js
@@ -2,15 +2,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { middleware, RESOLVED, PENDING, REJECTED } from '../util/middleware-decorator';
+import FallbackImage from '../components/FallbackImage';
 
 @connect(state => state)
 @middleware([])
 export default class extends React.Component {
 
     render() {
+        const { contestants, params } = this.props;
+        const contestant = contestants[params.contestant];
+
         return (
-            <div>
-                View Contestant
+            <div className="view-contestant">
+
+                <div className="stats">
+                    <div>
+                        <img src={contestant.data.images.body} />
+                        <h1>{contestant.data.name}</h1>
+                    </div>
+                    <div>
+                        <table>
+                            <tbody>
+                                {contestant.data.bioStats.map(data => (
+                                    <tr>
+                                        <td>{data.name}</td>
+                                        <td>{data.value}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <hr />
+                <div className="questions">
+                    {contestant.data.bioQuestions.map(data => (
+                        <div>
+                            <h3>{data.question}</h3>
+                            <p>{data.answer}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
